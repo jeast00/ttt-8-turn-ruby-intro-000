04a7712ae62c4de1c4122adb9b3f8fd882012b58
Jonathan East <jonathan_east@ymail.com>
2020-03-31 15:10:07 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,31 @@
+def display_board(board)
+  rows = [(" #{board[0]} | #{board[1]} | #{board[2]} "),
+          ("-----------"),
+          (" #{board[3]} | #{board[4]} | #{board[5]} "),
+          ("-----------"),
+          (" #{board[6]} | #{board[7]} | #{board[8]} ")]
+
+  puts rows[0]
+  puts rows[1]
+  puts rows[2]
+  puts rows[3]
+  puts rows[4]
+
+end
+
+def input_to_index(input)
+  user_input = input.to_i - 1
+end
+
+def move(board, index, value = "X")
+  board[index] = value
+end
+
+def valid_move?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+
+end
