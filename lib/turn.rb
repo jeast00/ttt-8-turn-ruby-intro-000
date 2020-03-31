8219d14a2378fdce8f7c9a8afe17731088c0e856
Jonathan East <jonathan_east@ymail.com>
2020-03-31 16:11:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,7 +19,7 @@ end
 
 def valid_move?(board, index)
   def position_taken?(board, index)
-    if board[index] == " " || board[index] == "" || board[index] == nil
+    if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
       return false
     else
       return true
