df2eca829f713aa86f12255f0824a518c71616dd
Jonathan East <jonathan_east@ymail.com>
2020-03-31 16:05:50 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,10 +18,11 @@ def input_to_index(input)
 end
 
 def valid_move?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil || !(board[index])
-    return true
-  else
-    return false
+  def position_taken?(board, index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      return false
+    else
+      return true
   end
 end
 
