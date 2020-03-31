4ab0977f63fd079be29c77eae1a08a9373a19dbd
Jonathan East <jonathan_east@ymail.com>
2020-03-31 16:26:41 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,4 +40,10 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   input_to_index(input)
+  if input
+    valid_move(board, index)
+    display_board(board)
+  else
+    puts "That number is invalid or has already been taken."
+    turn(board)
 end
