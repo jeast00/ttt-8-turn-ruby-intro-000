47ce229322a85678ff50c95417c417b8e3aa4be5
Jonathan East <jonathan_east@ymail.com>
2020-03-31 15:44:35 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -22,7 +22,7 @@ def move(board, index, value = "X")
 end
 
 def valid_move?(board, index)
-  if board[index].between?(1, 9)
+  if board[index].between?(1, 9).to_i
     return true
   else
     return false
